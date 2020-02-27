6d5c80b4ef1d2890af55510db426181d6282a351
Emil Bylykbashi <emilbylykbashi@gmail.com>
2020-02-27 23:31:31 +0200

    make it better

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const posts = require('../../posts');
+const posts = require('../../Posts');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 
