e94e916471f21c06853f2346b250c8203e71beb9
Emil Bylykbashi <emilbylykbashi@gmail.com>
2020-02-27 23:03:19 +0200

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
